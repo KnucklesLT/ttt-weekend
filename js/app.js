4bb4f3b804f5225f3a8aabcb811cbee37dcc1af3
Luigi T <luigitejada@yahoo.com>
2022-09-01 18:59:20 -0400

    add handleClick

--- js/app.js
+++ js/app.js
@@ -1,7 +1,14 @@
 /*-------------------------------- Constants --------------------------------*/
 const squareEls = document.querySelectorAll('.squares')
 const messageEl = document.getElementById('message')
-
+const winningCombos =[[0,1,2],
+                      [3,4,5],
+                      [6,7,8],
+                      [0,3,6],
+                      [1,4,7],
+                      [2,5,8],
+                      [0,4,8],
+                      [2,4,6]]
 
 
 
@@ -12,19 +19,19 @@ let board, turn, winner
 
 
 /*------------------------ Cached Element References ------------------------*/
-
+let boardSquares = document.querySelector('section')
 
 
 /*----------------------------- Event Listeners -----------------------------*/
 
-
+boardSquares.addEventListener('click',handleClick)
 
 /*-------------------------------- Functions --------------------------------*/
 
 init()
 
 function init(){
-  board = [1, -1, null, null, -1, null, null, -1, null]
+  board = [null, null, null, null, null, null, null, null, null]
   turn = 1
   winner = null
   render()
@@ -36,8 +43,10 @@ function render() {
   
     if(board[index]=== -1){
       element.style.backgroundColor = '#A9F0D1'
+      element.textContent = 'X'
     } else if (board[index] === 1){
       element.style.backgroundColor = '#FFA69E'
+      element.textContent = 'O'
     }else {
       element.style.backgroundColor = '#FFF7F8'
     }
@@ -50,4 +59,13 @@ function render() {
   } else {
     winner === 1 ? messageEl.textContent = `Player X wins!`  : messageEl.textContent = `Player O Wins!`
   }
+}
+
+function handleClick (evt){
+  sqIdx = parseInt(evt.target.id.replace('sq',''))
+  if(board[sqIdx] || !!winner) {
+    return
+  }
+  board[sqIdx] = turn
+  turn *= -1
 }
