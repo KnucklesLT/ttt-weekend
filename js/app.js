782d366f754c75b371b7bc9e1e3280d817be61d3
Luigi T <luigitejada@yahoo.com>
2022-09-01 22:10:10 -0400

    change button name

--- js/app.js
+++ js/app.js
@@ -20,13 +20,13 @@ let board, turn, winner
 
 /*------------------------ Cached Element References ------------------------*/
 let boardSquares = document.querySelector('section')
-let resetButton = document.querySelector('.reset-button')
+let resetBtnEl = document.querySelector('.reset-button')
 
 
 /*----------------------------- Event Listeners -----------------------------*/
 
 boardSquares.addEventListener('click',handleClick)
-resetButton.addEventListener('click', init)
+resetBtnEl.addEventListener('click', init)
 /*-------------------------------- Functions --------------------------------*/
 
 init()
